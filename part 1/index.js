96451e92af55a39dea4751862aa386705ac59067
Mujakkir Maher <mujakkirahmad78@gmail.com>
2026-08-25 15:13:56 +0600

    build basic Node.js HTTP server

---
+++ part 1/index.js
@@ -0,0 +1,35 @@
+/*
+ * Title: Uptime monitoring application
+ * Descrip: A RESTFul API to monitor up or down time of user difened links
+ * tutor: Sumit Saha
+ */
+
+
+// dependencies
+const http = require('http');
+
+// app object - module scaffolding
+const app = {};
+
+// configuration 
+app.config = {
+    port: 3000
+};
+
+// create server
+app.createServer = () => {
+    const server = http.createServer(app.handleReqRes);
+    // srever start korte hole server ke ekta port e listen korte hoy
+    server.listen(app.config.port, () => {
+        console.log(`listening to port ${app.config.port}`);
+    });
+};
+
+// handle request and response
+app.handleReqRes = (req, res) => {
+    // response handle
+    res.end('Hello ooworld');
+}
+
+// start the server
+app.createServer();
